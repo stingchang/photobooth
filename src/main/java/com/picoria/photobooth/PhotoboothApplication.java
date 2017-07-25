f6c54dd12fd9a96129c3f8ad2d869f022884cbd0
sting <sting.changh@gmail.com>
2017-07-24 21:57:41 -0700

    Initial commit

---
+++ src/main/java/com/picoria/photobooth/PhotoboothApplication.java
@@ -0,0 +1,12 @@
+package com.picoria.photobooth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoboothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoboothApplication.class, args);
+	}
+}
